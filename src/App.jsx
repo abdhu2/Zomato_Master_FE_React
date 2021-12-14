fd71824342ade8e949cadf5c064edb8a73da4c49
Abdalla Ibrahim <abdhu2628@gmail.com>
2021-12-14 20:19:34 +0530

    menu and photos added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,59 @@
 import "./App.css";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Redirect } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 
+// Hoc
 import HomeLayoutHoc from "./HOC/Home.hoc";
+import RestaurantLayoutHoc from "./HOC/Restaurant.hoc";
 
-//HOC
-import HomeLayout from "./layouts/Homepage.layout";
-
-//Pages
+// Pages
 import HomePage from "./pages/HomePage";
-
+import RestaurantPage from "./pages/RestaurantPage";
+import Overview from "./components/Restaurant/Overview";
+import OrderOnline from "./components/Restaurant/OrderOnline";
+import Reviews from "./components/Restaurant/Reviews/Reviews";
+import Menu from "./components/Restaurant/Menu/Menu";
+import Photos from "./components/Restaurant/Photos/Photos";
 
 function App() {
   return (
-      <>
-    
-        {/* <HomeLayoutHoc exact component={HomePage} path="/"/> */}
-        <Redirect from="/" to="/delivery" />
-        <HomeLayoutHoc exact component ={HomePage} path="/:type" />
-       
-      </>
+    <>
+      <Route path="/" exact>
+        <Redirect to="/delivery" />
+      </Route>
+      <HomeLayoutHoc path="/:type" exact component={HomePage} />
+      <RestaurantLayoutHoc
+        path="/restaurant/:id"
+        exact
+        component={RestaurantPage}
+      />
+      <RestaurantLayoutHoc
+        path="/restaurant/:id/overview"
+        exact
+        component={Overview}
+      />
+      <RestaurantLayoutHoc
+        path="/restaurant/:id/order-online"
+        exact
+        component={OrderOnline}
+      />
+      <RestaurantLayoutHoc
+        path="/restaurant/:id/reviews"
+        exact
+        component={Reviews}
+      />
+      <RestaurantLayoutHoc path="/restaurant/:id/menu" exact component={Menu} />
+      <RestaurantLayoutHoc
+        path="/restaurant/:id/photos"
+        exact
+        component={Photos}
+      />
+    </>
   );
 }
 
 export default App;
+
+//   /* NOTE: alternative redirect */
+//   /* <Redirect exact from="/" to="/delivery" /> */
